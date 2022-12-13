9b28f08f5a9fca7fdc91b0e072c8e7b8c3c10102
biruk kalewold <birukkalewoldasrat@gmail.com>
2022-12-13 22:53:58 +0300

    added functionality to parse and sotore job data from form

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -20,8 +23,25 @@ app.get("/post", function (req, res) {
   res.sendFile(htmlFilePath);
 });
 
+app.post("/post", function (req, res) {
+  const job = req.body;
+  console.log(job);
+  const filePath = path.join(__dirname, "data", "jobs.json");
+  const fileData = fs.readFileSync(filePath);
+  const postedJobs = JSON.parse(fileData);
+  postedJobs.push(job);
+  fs.writeFileSync(filePath, JSON.stringify(postedJobs));
+
+  res.redirect("/confirm");
+});
+
 app.get("/about", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "about.html");
+  res.sendFile(htmlFilePath);
+});
+
+app.get("/confirm", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "confirm.html");
   res.sendFile(htmlFilePath);
 });
 
