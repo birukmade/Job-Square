0676903a042ce675671f2d6ab308db5cf055602e
biruk kalewold <birukkalewoldasrat@gmail.com>
2022-12-13 21:23:51 +0300

    added route to /

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.get("/", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
+});
 app.listen(3000);
