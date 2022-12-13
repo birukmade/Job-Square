55da347a7146b27964c8244c41d198b040d6a546
biruk kalewold <birukkalewoldasrat@gmail.com>
2022-12-14 00:17:27 +0300

    set view and view engine options

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
